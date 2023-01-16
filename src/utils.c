abf984c1425cf951d028cacf346aec6cf70fc3c1
Adan Kvitschal <adan@moduhub.com>
2023-01-16 12:46:34 -0300

    Fixed flag check false trues

--- src/utils.c
+++ src/utils.c
@@ -3,11 +3,11 @@
 
 int utils_check_flag(const utils_flag_t flags, utils_flag_t mask)
 {
-    if (flags & mask != 0)
+    if (((flags) & (mask)) == 0)
     {
-        return 1;
+        return 0;
     }
-    return 0;
+    return 1;
 }
 
 void utils_set_flag(utils_flag_t *flags, utils_flag_t mask)
