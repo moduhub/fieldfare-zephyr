db04ac773c629e8a8036b855ed62262f150bc8e9
Adan Kvitschal <adan@moduhub.com>
2023-01-18 17:41:39 -0300

    Added js backtrace handler

--- src/jerry/jerry_main.c
+++ src/jerry/jerry_main.c
@@ -9,6 +9,36 @@
 
 jz_timeout_list_entry *timeout_list_ptr;
 
+static jerry_value_t
+jz_backtrace_handler (  const jerry_call_info_t *call_info_p,
+                        const jerry_value_t args_p[],
+                        const jerry_length_t args_count)
+{   
+    if (!jerry_feature_enabled (JERRY_FEATURE_LINE_INFO))
+        printk("JERRY_FEATURE_LINE_INFO is disabled, no backtrace information available.\n");
+    } else {
+        printk("backtrace start ---------\n");
+        jerry_value_t backtrace_array = jerry_backtrace (5);
+        uint32_t array_length = jerry_array_length (backtrace_array);
+        printk("jz_print_backtrace array_length=%d", array_length);
+        for (uint32_t idx = 0; idx < array_length; idx++)
+        {
+            jerry_value_t property = jerry_object_get_index (backtrace_array, idx);
+            jerry_char_t string_buffer[64];
+            jerry_size_t copied_bytes = jerry_string_to_buffer (property,
+                                                                JERRY_ENCODING_UTF8,
+                                                                string_buffer,
+                                                                sizeof (string_buffer) - 1);
+            string_buffer[copied_bytes] = '\0';
+            printk(" %d: %s\n", idx, string_buffer);
+            jerry_value_free (property);
+        }
+        printk("backtrace end ---------\n");
+        jerry_value_free (backtrace_array);
+    }
+    return jerry_undefined();
+}
+
 int jz_load_user_code (void)
 {
     const jerry_char_t script[] = "print('Hello from JavaScript');\
@@ -20,19 +50,29 @@ int jz_load_user_code (void)
             print('js count: ' + count);\
             count++;\
         }\
+        function func_a (message) {\
+            print('func_a called');\
+            print('message inside func is: ' + message);\
+            backtrace();\
+        };\
+        function func_b() {\
+            print('func b called');\
+            func_a('xa');\
+        };\
         setTimeout(() => {\
             print('10 seconds timeout!');\
         }, 10000);\
         setInterval(() => {\
             print('5 seconds interval');\
         }, 5000);\
+        func_b();\
 	";
-
 	const jerry_length_t script_size = sizeof (script) - 1;
-
-	/* Setup Global scope code */
+    jerry_parse_options_t parse_options;
+    parse_options.options = JERRY_PARSE_HAS_SOURCE_NAME;
+    parse_options.source_name = jerry_string_sz (".snapshot");
 	jerry_value_t parsed_code = jerry_parse (script, script_size, NULL);
-
+    jerry_value_free (parse_options.source_name);
 	if (!jerry_value_is_exception (parsed_code))
 	{
 		/* Execute the parsed source code in the Global scope */
@@ -89,6 +129,7 @@ void jz_register_handlers()
 {
 
     jerryx_register_global ("print", jerryx_handler_print);
+    jerryx_register_global ("backtrace", jz_backtrace_handler);
     //jerryx_register_global("fs_init", fs_init_handler);
 
     jerryx_register_global ("setTimeout", jz_timeout_handler);
